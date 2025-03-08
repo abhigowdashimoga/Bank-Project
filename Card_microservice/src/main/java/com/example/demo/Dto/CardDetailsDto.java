68f09e77ac9a8bcfead42acd8bab94a1d04cccaa
abhigowdashimoga <123055280+abhigowdashimoga@users.noreply.github.com>
2025-03-08 19:05:27 +0530

    Add files via upload

---
+++ Card_microservice/src/main/java/com/example/demo/Dto/CardDetailsDto.java
@@ -0,0 +1,20 @@
+package com.example.demo.Dto;
+
+import lombok.Data;
+
+@Data
+public class CardDetailsDto {
+	
+    private String name;
+	
+	private String email;
+	
+	private String phoneno;
+	
+	private Long Accountno;
+	
+    private Long cardno;
+	
+	private int balance;
+
+}
---
+++ Card_microservice/src/main/java/com/example/demo/Entity/Card.java
@@ -0,0 +1,30 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Card {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String phoneno;
+	
+	private Long Accountno;
+	
+	private Long cardno;
+	
+	private int balance;
+
+}
