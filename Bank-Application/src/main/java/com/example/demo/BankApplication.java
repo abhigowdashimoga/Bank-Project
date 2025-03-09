f7ba9d3d8bad8dc1fffa10e9e33fa7b9fa01a4fd
abhigowdashimoga <123055280+abhigowdashimoga@users.noreply.github.com>
2025-03-09 10:05:43 +0530

    commit

--- Bank-Application/src/main/java/com/example/demo/BankApplication.java
+++ Bank-Application/src/main/java/com/example/demo/BankApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "AuditAware")
 @OpenAPIDefinition(
 		info = @Info(title="Bank Microservice",
